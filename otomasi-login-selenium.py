ab242cded6a0f3bc74bc082787204be188bc69f0
Ihsan Fadli Tampati <ifat.ichan@gmail.com>
2024-02-05 08:32:41 +0700

    perbaikan nomor 2: Otomasi login

--- otomasi-login-selenium.py
+++ otomasi-login-selenium.py
@@ -12,29 +12,32 @@ class LoginTestCase(unittest.TestCase):
         self.browser.get('http://localhost/ihsanfati-uas-ppl-BadCRUD/login.php')
         expected_result = "Please sign in"
         actual_result = self.browser.title
-        self.assertIn(expected_result, actual_result)
+        self.assertIn(expected_result, self.browser.page_source, f"'{expected_result}' not found in page source")
 
     def test_2_invalid_username(self):
         self.browser.find_element(By.ID, "inputUsername").send_keys("usernametidakbenar")
         self.browser.find_element(By.ID, "inputPassword").send_keys("minda666!")
         self.browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         expected_result = "Wrong username or password"
-        actual_result = self.browser.find_element(By.XPATH, "//div[@class='checkbox mb-3']/label").text
-        self.assertIn(expected_result, actual_result)
+        actual_result = self.browser.find_element(By.XPATH, "//div[@class='checkbox mb-3']/label").text.strip()
+        self.assertIn(expected_result, self.browser.page_source, f"'{expected_result}' not found in page source")
 
     def test_3_invalid_password(self):
         self.browser.find_element(By.ID, "inputUsername").send_keys("admin")
         self.browser.find_element(By.ID, "inputPassword").send_keys("passwordtidakbenar")
         self.browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         expected_result = "Wrong username or password"
-        actual_result = self.browser.find_element(By.XPATH, "//div[@class='checkbox mb-3']/label").text
-        self.assertIn(expected_result, actual_result)
+        actual_result = self.browser.find_element(By.XPATH, "//div[@class='checkbox mb-3']/label").text.strip()
+        self.assertIn(expected_result, self.browser.page_source, f"'{expected_result}' not found in page source")
 
     def test_4_valid_credentials(self):
         self.browser.find_element(By.ID, "inputUsername").send_keys("admin")
         self.browser.find_element(By.ID, "inputPassword").send_keys("minda666!")
         self.browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
-        expected_url = "http://localhost/ihsanfati-uas-ppl-BadCRUD/index.php"
+        expected_url = "http://localhost/ihsanfati-uas-ppl-BadCRUD/index.php" if "Wrong usename or password" not in self.browser.page_source else "http://localhost/ihsanfati-uas-ppl-BadCRUD/login.php"
+        expected_url = expected_url.strip()
+            # Memeriksa apakah expected_url terdapat di dalam current_url
+        self.assertIn(expected_url, self.browser.current_url)
         self.assertEqual(expected_url, self.browser.current_url)
 
     @classmethod
